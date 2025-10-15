8483069c34f8e64018d962466f5cbdbf84e70bd4
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2025-10-15 13:50:21 -0300

    [FEAT] Implementando barra de navegação responsiva

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule], // <- aqui está o truque
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#de2b2b"
+      }
+    },
   },
   plugins: [],
 }
