e7884c13bb25a800a178dc4475976239c0845bf7
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2025-10-18 02:26:18 -0300

    [FEAT] Criando a página de categorias e componente header dinâmico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'categorias',
+        component: CategoriesComponent
     }
 ];
---
+++ src/app/pages/categories/categories.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { PageHeaderComponent } from "../../components/page-header/page-header.component";
+import { TmdbService } from '../../services/tmdb.service';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [PageHeaderComponent],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.css'
+})
+export class CategoriesComponent {
+
+  constructor(private tmdbService: TmdbService) {}
+  
+  banner?: String | null
+  number = Math.floor(Math.random() * 20);
+
+ngOnInit() {
+    this.tmdbService.getPopularMovies().subscribe(res => {
+      this.banner = res.results[this.number].backdrop_path;
+      console.log(this.banner)
+    });
+  }
+}
